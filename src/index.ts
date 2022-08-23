2f1fe5af3e2257e168279c3ffc9b8c1907a7252b
Shay Keren <shay.keren@gmail.com>
2022-08-23 21:30:36 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,288 @@
+import type * as express from 'express';
+import * as core from 'express-serve-static-core';
+import { trace } from '@opentelemetry/api';
+import { SemanticAttributes } from '@opentelemetry/semantic-conventions';
+import { name } from '../package.json';
+
+const getFunctionLocation = require('get-function-location');
+export interface RouteDetails {
+    filePath: string;
+    function: string;
+    route: string;
+    line: number;
+    // regexp: string;
+}
+const routesMap: Record<string, RouteDetails> = {};
+
+// function split (thing: any): string [] {
+//     if (typeof thing === 'string') {
+//       return thing.split('/')
+//     } else if (thing.fast_slash) {
+//       return []
+//     } else {
+//       var match = thing.toString()
+//         .replace('\\/?', '')
+//         .replace('(?=\\/|$)', '$')
+//         .match(/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//)
+//       return match
+//         ? match[1].replace(/\\(.)/g, '$1').split('/')
+//         : '<complex:' + thing.toString() + '>'
+//     }
+//   }
+
+// function iterate (path: string [] = [], layer: any) {
+//     if (layer.route) {
+//         for(let l of layer.route.stack){
+//             const paths = split(layer.route.path);
+//             iterate(path.concat(paths), l);
+//         }
+//         return;
+//     }
+//     if (layer.name === 'router') {
+//         let stack;
+//         if (layer.handle.__original.stack) {
+//             stack = layer.handle.__original.stack;
+//         }
+//         else {
+//             stack = layer.handle.stack;
+//         }
+//         if (stack) {
+//             for(let l of stack){
+//                 iterate(path.concat(split(layer.regexp)), l);
+//             }
+//             return;
+//         }
+//     }
+//     if (layer.method) {
+//         const routePath = '/'+path.concat(split(layer.regexp)).filter(Boolean).join('/');
+//         routesMap[routePath] = {
+//             filePath: "",
+//             function: layer.method,
+//             route: routePath,
+//             //regexp: layer.regexp,
+//             line:-1
+//         }
+//         console.log('%s %s', layer.method, routePath);
+//     }
+//   }
+
+// export function discover(app: express.Application){
+//     for(let l of app._router.stack){
+//         iterate([],l);
+//     }
+// }
+
+export function useDigmaRouterMiddleware(router: express.Router) {
+    router.use(function (req, res, next) {
+        const tracer = trace.getTracerProvider().getTracer(name);
+        tracer.startActiveSpan('middleware - digma', span => {
+            console.log("router middleware digma - before");
+            next();
+            console.log("router middleware digma - after");
+            if (req.route) {
+                const route = req.baseUrl + req.route.path;
+                let routeDetails = routesMap[route];
+                if (!routeDetails) {
+                    const layer = req.route.stack[0];
+                    const lp = getFunctionLocation(layer.handle);
+                    lp.then((location: { source: any; line: any; })=>{
+                        console.log("location:", location);
+                        if (location) {
+                            routesMap[route] = {
+                                filePath: location.source,
+                                line: location.line,
+                                function: layer.name,
+                                route: route
+                            }
+                        }
+                    });
+                   
+                }
+                routeDetails = routesMap[route]
+                if (routeDetails) {
+                    span.setAttribute(SemanticAttributes.CODE_FILEPATH, routeDetails.filePath);
+                    span.setAttribute(SemanticAttributes.CODE_LINENO, routeDetails.line);
+                    span.setAttribute(SemanticAttributes.CODE_FUNCTION, routeDetails.function);
+                }
+                span.end();
+            }
+
+        });
+    })
+}
+
+export function useDigmaRouterMiddlewareAsync(router: express.Router) {
+    router.use(async function (req, res, next) {
+        const tracer = trace.getTracerProvider().getTracer(name);
+        await tracer.startActiveSpan('middleware - digma', async span => {
+            console.log("router middleware digma - before");
+            next();
+            console.log("router middleware digma - after");
+            if (req.route) {
+                const route = req.baseUrl + req.route.path;
+                let routeDetails = routesMap[route];
+                if (!routeDetails) {
+                    const layer = req.route.stack[0];
+                    const location = await getFunctionLocation(layer.handle);
+                    console.log("location:", location);
+                    if (location) {
+                        routesMap[route] = {
+                            filePath: location.source,
+                            line: location.line,
+                            function: layer.name,
+                            route: route
+                        }
+                    }
+                }
+                routeDetails = routesMap[route]
+                if (routeDetails) {
+                    span.setAttribute(SemanticAttributes.CODE_FILEPATH, routeDetails.filePath);
+                    span.setAttribute(SemanticAttributes.CODE_LINENO, routeDetails.line);
+                    span.setAttribute(SemanticAttributes.CODE_FUNCTION, routeDetails.function);
+                }
+                span.end();
+            }
+
+        });
+    })
+}
+export function useDigmaAppMiddleware(app: express.Application) {
+    app.use((req, res, next) => {
+        const tracer = trace.getTracerProvider().getTracer(name);
+        tracer.startActiveSpan('middleware - digma', span => {
+            console.log("app middleware digma - before");
+            next();
+            console.log("app middleware digma - after");
+            const route = req.baseUrl + req.route.path;
+            let routeDetails = routesMap[route];
+            if (!routeDetails) {
+                const layer = req.route.stack[0];
+                const lp = getFunctionLocation(layer.handle);
+                lp.then((location: { source: any; line: any; })=>{
+                    console.log("location:", location);
+                    if (location) {
+                        routesMap[route] = {
+                            filePath: location.source,
+                            line: location.line,
+                            function: layer.name,
+                            route: route
+                        }
+                    }
+                });
+            }
+            routeDetails = routesMap[route]
+            if (routeDetails) {
+                span.setAttribute(SemanticAttributes.CODE_FILEPATH, routeDetails.filePath);
+                span.setAttribute(SemanticAttributes.CODE_LINENO, routeDetails.line);
+                span.setAttribute(SemanticAttributes.CODE_FUNCTION, routeDetails.function);
+            }
+            span.end();
+        });
+    });
+}
+
+export function useDigmaAppMiddlewareAsync(app: express.Application) {
+    app.use(async (req, res, next) => {
+        const tracer = trace.getTracerProvider().getTracer(name);
+        await tracer.startActiveSpan('middleware - digma', async span => {
+            console.log("app middleware digma - before");
+            next();
+            console.log("app middleware digma - after");
+            const route = req.baseUrl + req.route.path;
+            let routeDetails = routesMap[route];
+            if (!routeDetails) {
+                const layer = req.route.stack[0];
+                const location = await getFunctionLocation(layer.handle);
+                if (location) {
+                    routesMap[route] = {
+                        filePath: location.source,
+                        line: location.line,
+                        function: layer.name,
+                        route: route
+                    }
+                }
+            }
+            routeDetails = routesMap[route]
+            if (routeDetails) {
+                span.setAttribute(SemanticAttributes.CODE_FILEPATH, routeDetails.filePath);
+                span.setAttribute(SemanticAttributes.CODE_LINENO, routeDetails.line);
+                span.setAttribute(SemanticAttributes.CODE_FUNCTION, routeDetails.function);
+            }
+            span.end();
+
+
+        });
+    });
+
+    // export function useDigmaMiddleware(app: express.Application){
+    //     app.use( async (req, res, next) => {
+    //         const tracer = trace.getTracerProvider().getTracer(name);
+    //         return await tracer.startActiveSpan('middleware - digma', async span=>{
+    //             try{
+    //                 return next();
+    //             }
+    //             catch(e){
+    //                 throw e;
+    //             }
+    //             finally{
+    //                 const route = req.baseUrl + req.route.path;
+    //                 let routeDetails = routesMap[route];
+    //                 if(!routeDetails){
+    //                     const layer = req.route.stack[0];
+    //                     const location =  await getFunctionLocation(layer.handle);
+    //                     if(location){
+    //                         routesMap[route] = {
+    //                             filePath: location.source,
+    //                             line: location.line,
+    //                             function: layer.name,
+    //                             route: route
+    //                         }
+    //                     }
+    //                 }
+    //                 routeDetails = routesMap[route]
+    //                 if(routeDetails){
+    //                     span.setAttribute(SemanticAttributes.CODE_FILEPATH, routeDetails.filePath);
+    //                     span.setAttribute(SemanticAttributes.CODE_LINENO, routeDetails.line);
+    //                     span.setAttribute(SemanticAttributes.CODE_FUNCTION, routeDetails.function);
+    //                 }
+    //                 span.end();
+    //             }
+
+    //         });
+    //        });
+
+    // router.use(async function (req, res, next) {
+    //     // let rootSpan = trace.getSpan(context.active());
+    //     // if(rootSpan){
+    //     //     rootSpan.setAttribute("test", "asd");
+    //     // }
+    //     const tracer = trace.getTracerProvider().getTracer("DIGMAAAA");
+    //     await tracer.startActiveSpan("source code",async span=>{
+    //         next();
+    //         const route = req.baseUrl + req.route.path;
+    //         let routeDetails = routesMap[route];
+    //         if(!routeDetails){
+    //             const layer = req.route.stack[0];
+    //             const location =  await getFunctionLocation(layer.handle);
+    //             console.log("location:", location);
+    //             if(location){
+    //                 routesMap[route] = {
+    //                     filePath: location.source,
+    //                     line: location.line,
+    //                     function: layer.name,
+    //                     regexp: "",
+    //                     route: route
+    //                 }
+    //             }
+    //         }
+    //         routeDetails = routesMap[route]
+    //         if(routeDetails){
+    //             span.setAttribute(SemanticAttributes.CODE_FILEPATH, routeDetails.filePath);
+    //             span.setAttribute(SemanticAttributes.CODE_LINENO, routeDetails.line);
+    //             span.setAttribute(SemanticAttributes.CODE_FUNCTION, routeDetails.function);
+    //         }
+    //         span.end();
+    //     });
+    // })
+}
+
