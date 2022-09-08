ba283015b1f85c5dffc6375c919a5df0c2c4036f
Noam Kfir <nkfir@digma.ai>
2022-09-08 15:19:12 +0300

    Capture errors, remove experimental handlers, return a conventional middleware function instead of taking a router, rearrange the code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import { Router } from 'express';
-import { trace, context, Span } from '@opentelemetry/api';
+import { trace, context, Span, SpanStatusCode } from '@opentelemetry/api';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { SemanticAttributes } from '@opentelemetry/semantic-conventions';
 import getFunctionLocation from 'get-function-location';
 
-export interface RouteDetails {
+interface RouteDetails {
     filePath: string;
     function: string;
     route: string;
@@ -13,16 +12,46 @@ export interface RouteDetails {
 
 const routesMap = new Map<string, RouteDetails>();
 
-function setSemanticAttributesForRoute(span?: Span, routeDetails?: RouteDetails) {
-    if(routeDetails) {
-        span?.setAttribute(SemanticAttributes.CODE_FILEPATH, routeDetails.filePath);
-        span?.setAttribute(SemanticAttributes.CODE_LINENO, routeDetails.line);
-        span?.setAttribute(SemanticAttributes.CODE_FUNCTION, routeDetails.function);
+export function applyDigmaInstrumentation(sdk: NodeSDK) {
+    process.on('uncaughtException', handleUncaughtException);
+    
+    setHookToApplySemanticAttributes(sdk);
+}
+
+export async function digmaRouteHandler(req, res, next) {
+    const span = getActiveSpan();
+    try {
+        next();
+        if (req.route) {
+            await handleRoute(req, span);
+        }
+    }
+    catch (error) {
+        next(error);
     }
 }
 
-function getRequestId(request) {
-    return `${request.method}:${request.baseUrl}${request.path}`;
+function setHookToApplySemanticAttributes(sdk: NodeSDK) {
+    const httpInstrumentation = sdk
+        // @ts-ignore: we need access to the private member _instrumentations
+        ._instrumentations[0]
+        .find(i => i.instrumentationName === '@opentelemetry/instrumentation-http');
+
+    const existingHook = httpInstrumentation.getConfig().applyCustomAttributesOnSpan;
+
+    httpInstrumentation.setConfig({
+        applyCustomAttributesOnSpan: function (span, request, response) {
+            if (existingHook) {
+                existingHook.call(httpInstrumentation, span, request, response);
+            }
+
+            const requestId = getRequestId(request);
+            if (routesMap.has(requestId)) {
+                const routeInfo = routesMap.get(requestId);
+                setSemanticAttributesForRoute(span, routeInfo);
+            }
+        },
+    });
 }
 
 async function handleRoute(req, span: Span | undefined) {
@@ -42,36 +71,33 @@ async function handleRoute(req, span: Span | undefined) {
     }
 }
 
-export function useDigmaRouterMiddleware(router: Router) {
-    router.use(async function (req, res, next) {
-        const activeContext = context.active();
-        const rootSpan = trace.getSpan(activeContext);
-        next();
-        if (req.route) {
-            await handleRoute(req, rootSpan);
-        }
-    })
+function getActiveSpan(): Span | undefined {
+    const activeContext = context.active();
+    const span = trace.getSpan(activeContext);
+    return span;
 }
 
-export function applyDigmaInstrumentation(sdk: NodeSDK) {
-    const httpInstrumentation = sdk
-        // @ts-ignore: we need access to the private member _instrumentations
-        ._instrumentations[0]
-        .find(i => i.instrumentationName === '@opentelemetry/instrumentation-http');
+function getRequestId(request) {
+    return `${request.method}:${request.baseUrl}${request.path}`;
+}    
 
-    const existingHook = httpInstrumentation.getConfig().applyCustomAttributesOnSpan;
+function setSemanticAttributesForRoute(span?: Span, routeDetails?: RouteDetails) {
+    if(routeDetails) {
+        span?.setAttribute(SemanticAttributes.CODE_FILEPATH, routeDetails.filePath);
+        span?.setAttribute(SemanticAttributes.CODE_LINENO, routeDetails.line);
+        span?.setAttribute(SemanticAttributes.CODE_FUNCTION, routeDetails.function);
+    }    
+}    
 
-    httpInstrumentation.setConfig({
-        applyCustomAttributesOnSpan: function (span, request, response) {
-            if(existingHook) {
-                existingHook.call(httpInstrumentation, span, request, response);
-            }
-
-            const requestId = getRequestId(request);
-            if(routesMap.has(requestId)) {
-                const routeInfo = routesMap.get(requestId);
-                setSemanticAttributesForRoute(span, routeInfo);
-            }
-        },
-    });
+function handleUncaughtException(err) {
+    const span = getActiveSpan();
+    if(span) {
+        try {
+            span.recordException(err);
+            span.setStatus({ code: SpanStatusCode.ERROR, message: err.message });
+        }
+        finally {
+            span.end();
+        }
+    }
 }
